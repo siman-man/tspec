2846444fd3358f115d473b31e5b411c8b746f188
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-02-18 13:22:38 +0900

    spec execute randomly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
+  config.order = :random
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
